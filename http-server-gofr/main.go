d4dc2f955e173cadee378c4cd06e324d075e41b1
Adityaa Mehra <adirakash@gmail.com>
2024-09-15 19:21:07 +0530

    final

--- http-server-gofr/main.go
+++ http-server-gofr/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "gofr.dev/pkg/gofr"
 
+func eventhandler(*gofr.Context) (interface{}, error) {
+	return "Welcome to gofr server", nil
+}
+
+func main() {
+	app := gofr.New() // App is of the type GoFR app
+
+	app.GET("/event", eventhandler)
+	app.POST("/event", eventhandler)
+	app.Run()
 }
